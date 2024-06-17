44eef2ffcb68ee08bc74ed666deeeb32ca436e68
Tech Hara <techhara@outlook.com>
2024-06-17 13:37:51 -0400

    use bufwriter for stdout

--- src/bin/gunzip.rs
+++ src/bin/gunzip.rs
@@ -1,10 +1,10 @@
-use std::io::{stdin, stdout, Result};
+use std::io::{stdin, stdout, BufWriter, Result};
 
 use flate2::bufread::MultiGzDecoder;
 
 fn main() -> Result<()> {
     let read = stdin().lock();
-    let mut write = stdout();
+    let mut write = BufWriter::new(stdout());
 
     let mut decoder = MultiGzDecoder::new(read);
     std::io::copy(&mut decoder, &mut write)?;
--- src/bin/gzip.rs
+++ src/bin/gzip.rs
@@ -1,10 +1,10 @@
-use std::io::{stdin, stdout, Result};
+use std::io::{stdin, stdout, BufWriter, Result};
 
 use flate2::{bufread::GzEncoder, Compression};
 
 fn main() -> Result<()> {
     let read = stdin().lock();
-    let mut write = stdout();
+    let mut write = BufWriter::new(stdout());
 
     let mut encoder = GzEncoder::new(read, Compression::best());
     std::io::copy(&mut encoder, &mut write)?;
