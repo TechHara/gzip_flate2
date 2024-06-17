e6c2e4f6fd78f10d6022614651d24cd606f59f75
Tech Hara <techhara@outlook.com>
2024-06-17 13:03:05 -0400

    use multi decoder

--- src/bin/gunzip.rs
+++ src/bin/gunzip.rs
@@ -1,12 +1,12 @@
 use std::io::{stdin, stdout, Result};
 
-use flate2::bufread::GzDecoder;
+use flate2::bufread::MultiGzDecoder;
 
 fn main() -> Result<()> {
     let read = stdin().lock();
     let mut write = stdout();
 
-    let mut decoder = GzDecoder::new(read);
+    let mut decoder = MultiGzDecoder::new(read);
     std::io::copy(&mut decoder, &mut write)?;
     Ok(())
 }
